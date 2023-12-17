9599676f26f0fa20c40f1732b0655ec6a7117cbe
b.serafimov <b.b.serafimov@gmail.com>
2023-12-17 20:42:24 +0200

    fix

--- BookStore2023/BookStore2023.BL/Interfaces/IAutorService.cs
+++ BookStore2023/BookStore2023.BL/Interfaces/IAutorService.cs
@@ -1,17 +1,13 @@
-﻿
-
-using BookStore2023.Models.Model;
+﻿using BookStore2023.Models.Model;
 
 namespace BookStore2023.BL.Interfaces
 {
     internal interface IAutorService
     {
-      
             void Add(Autor autor);
             void Delete(int Id);
             Autor? GetById(int Id);
             List<Autor> GetAll();
             Autor? Update(Autor autor);
-       
     }
 }
